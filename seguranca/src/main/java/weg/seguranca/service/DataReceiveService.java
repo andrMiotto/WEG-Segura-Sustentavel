49031d4ed5a1ef5b6aa77ae898fe1106ad2c8eeb
DANIEL VINICIUS RIOS SISMER <daniel_sismer@estudante.sesisenai.org.br>
2025-08-20 18:19:55 -0300

    MIDS-05: adiciona base controller e service

--- seguranca/src/main/java/weg/seguranca/service/DataReceiveService.java
+++ seguranca/src/main/java/weg/seguranca/service/DataReceiveService.java
@@ -1,4 +1,33 @@
 package weg.seguranca.service;
 
+import weg.seguranca.repository.MySQLRepository;
+import weg.seguranca.repository.NoSQLRepository;
+
+import java.util.List;
+
 public class DataReceiveService {
+
+    private final MySQLRepository mySQLRepository;
+    private final NoSQLRepository noSQLRepository;
+
+    public DataReceiveService(MySQLRepository mySQLRepository, NoSQLRepository noSQLRepository) {
+        this.mySQLRepository = mySQLRepository;
+        this.noSQLRepository = noSQLRepository;
+    }
+
+    public void importData(){
+        /* Aguardar a equipe reponsável pelos models e DTO*/
+        /*=============
+        *   Prévia
+        *=============
+        *
+        * List <OBJETO-DOCUMENT> documents = noSQLRepository.findAll(); --- Método responsável por encontrar todos os dados do objeto especificado
+        * List <OBJETO-ENTIDADE> entidadesTratadas = documents.stream()... ---- Rastrea e ttratar valores, aplicando a regra de negócio
+        *
+        * MySQLRepository.SaveAll(entidadesTratadas); --- Salva no MySQL os dados "tratados"
+        * */
+    }
+
+    /* */
+    // -
 }

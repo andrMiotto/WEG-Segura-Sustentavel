c5b0c394a7b74344467b43fb1cf411ba9fe365b0
hugo <hugo_paim@estudante.sesisenai.org.br>
2025-10-01 16:57:31 -0300

    feat: cria método de update de pessoa por sala atual

--- seguranca/src/main/java/weg/seguranca/dao/SalaEmergenciaDao.java
+++ seguranca/src/main/java/weg/seguranca/dao/SalaEmergenciaDao.java
@@ -15,7 +15,7 @@ public class SalaEmergenciaDao {
     }
 
     public void salvar(SalaEmergencia salaEmergencia) throws SQLException {
-        String sql = "INSERT INTO sala_emergencia (sala_id, emergencia_id) VALUES (?, ?)";
+        String sql = "INSERT INTO salas_emergencia (sala_id, emergencia_id) VALUES (?, ?)";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, salaEmergencia.getSala_id());
             stmt.setInt(2, salaEmergencia.getEmergencia_id());
@@ -24,7 +24,7 @@ public class SalaEmergenciaDao {
     }
 
     public SalaEmergencia buscarPorId(int id) throws SQLException {
-        String sql = "SELECT * FROM sala_emergencia WHERE id = ?";
+        String sql = "SELECT * FROM salas_emergencia WHERE id = ?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, id);
             try (ResultSet rs = stmt.executeQuery()) {
@@ -42,7 +42,7 @@ public class SalaEmergenciaDao {
 
     public List<SalaEmergencia> listarTodos() throws SQLException {
         List<SalaEmergencia> lista = new ArrayList<>();
-        String sql = "SELECT * FROM sala_emergencia";
+        String sql = "SELECT * FROM salas_emergencia";
         try (Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
             while (rs.next()) {
@@ -57,7 +57,7 @@ public class SalaEmergenciaDao {
     }
 
     public void atualizar(SalaEmergencia salaEmergencia) throws SQLException {
-        String sql = "UPDATE sala_emergencia SET sala_id=?, emergencia_id=? WHERE id=?";
+        String sql = "UPDATE salas_emergencia SET sala_id=?, emergencia_id=? WHERE id=?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, salaEmergencia.getSala_id());
             stmt.setInt(2, salaEmergencia.getEmergencia_id());
@@ -67,7 +67,7 @@ public class SalaEmergenciaDao {
     }
 
     public void deletar(int id) throws SQLException {
-        String sql = "DELETE FROM sala_emergencia WHERE id=?";
+        String sql = "DELETE FROM salas_emergencia WHERE id=?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, id);
             stmt.executeUpdate();
